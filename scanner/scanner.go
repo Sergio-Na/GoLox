cfb2873c2d33a5087e7754992764f5a5b435d833
Sergio Najm <98653927+Sergio-Na@users.noreply.github.com>
2023-12-03 22:38:38 -0500

    Moved from string to byte in scanner

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -17,84 +17,83 @@ func New(source string) Scanner {
 	scanner := Scanner{source: source, tokens: make([]token.Token, 0), start: 0, current: 0, line: 1}
 	return scanner
 }
+
 func (scanner *Scanner) setSource(source string) {
 	scanner.source = source
 }
+
 func (scanner *Scanner) scanTokens() {
-	for scanner.isAtEnd() {
+	for !scanner.isAtEnd() {
 		scanner.start = scanner.current
 		scanner.scanToken()
 	}
 	scanner.tokens = append(scanner.tokens, token.Token{Type: token.EOF, Lexeme: "", Literal: nil, Line: scanner.line})
 }
+
 func (scanner *Scanner) isAtEnd() bool {
 	return scanner.current >= len(scanner.source)
 }
+
 func (scanner *Scanner) scanToken() {
 	char := scanner.advance()
 	switch char {
-	case "(":
+	case '(':
 		scanner.addToken(token.LEFT_PAREN)
-	case ")":
-		scanner.addToken(token.LEFT_PAREN)
-	case "{}":
+	case ')':
+		scanner.addToken(token.RIGHT_PAREN)
+	case '{':
 		scanner.addToken(token.LEFT_BRACE)
-	case "}":
+	case '}':
 		scanner.addToken(token.RIGHT_BRACE)
-	case ".":
+	case '.':
 		scanner.addToken(token.DOT)
-	case ",":
+	case ',':
 		scanner.addToken(token.COMMA)
-	case "-":
+	case '-':
 		scanner.addToken(token.MINUS)
-	case "+":
+	case '+':
 		scanner.addToken(token.PLUS)
-	case ";":
+	case ';':
 		scanner.addToken(token.SEMICOLON)
-	case "*":
+	case '*':
 		scanner.addToken(token.STAR)
-	case "!":
+	case '!':
 		if scanner.match('=') {
 			scanner.addToken(token.BANG_EQUAL)
 		} else {
 			scanner.addToken(token.BANG)
 		}
-	case "=":
+	case '=':
 		if scanner.match('=') {
 			scanner.addToken(token.EQUAL_EQUAL)
 		} else {
 			scanner.addToken(token.EQUAL)
 		}
-	case "<":
+	case '<':
 		if scanner.match('=') {
 			scanner.addToken(token.LESS_EQUAL)
 		} else {
 			scanner.addToken(token.LESS)
 		}
-	case ">":
+	case '>':
 		if scanner.match('=') {
 			scanner.addToken(token.GREATER_EQUAL)
 		} else {
 			scanner.addToken(token.GREATER)
 		}
-	case "/":
+	case '/':
 		if scanner.match('/') {
-			for scanner.peek() != "\n" && !scanner.isAtEnd() {
+			for scanner.peek() != '\n' && !scanner.isAtEnd() {
 				scanner.advance()
 			}
 		} else {
 			scanner.addToken(token.SLASH)
 		}
-	case " ":
-
-	case "\r":
-	case "\t":
+	case ' ', '\r', '\t':
 		// Ignore whitespace.
-		break
-
-	case "\n":
+	case '\n':
 		scanner.line++
-	case "\"":
+	case '"':
 		scanner.addString()
 	default:
 		if isDigit(char) {
@@ -104,18 +103,22 @@ func (scanner *Scanner) scanToken() {
 		}
 	}
 }
-func (scanner *Scanner) advance() string {
-	char := string(scanner.source[scanner.current])
+
+func (scanner *Scanner) advance() byte {
+	char := scanner.source[scanner.current]
 	scanner.current++
 	return char
 }
+
 func (scanner *Scanner) addToken(tp token.Type) {
 	scanner.addTokenWithLiteral(tp, nil)
 }
+
 func (scanner *Scanner) addTokenWithLiteral(tp token.Type, literal interface{}) {
 	text := scanner.source[scanner.start:scanner.current]
 	scanner.tokens = append(scanner.tokens, token.Token{Type: tp, Lexeme: text, Literal: literal, Line: scanner.line})
 }
+
 func (scanner *Scanner) match(expected byte) bool {
 	if scanner.isAtEnd() {
 		return false
@@ -126,15 +129,17 @@ func (scanner *Scanner) match(expected byte) bool {
 	scanner.current++
 	return true
 }
-func (scanner *Scanner) peek() string {
+
+func (scanner *Scanner) peek() byte {
 	if scanner.isAtEnd() {
-		return "\\0"
+		return 0 // Null character
 	}
-	return string(scanner.source[scanner.current])
+	return scanner.source[scanner.current]
 }
+
 func (scanner *Scanner) addString() {
-	for scanner.peek() != "\"" && !scanner.isAtEnd() {
-		if scanner.peek() == "\n" {
+	for scanner.peek() != '"' && !scanner.isAtEnd() {
+		if scanner.peek() == '\n' {
 			scanner.line++
 		}
 		scanner.advance()
@@ -143,28 +148,32 @@ func (scanner *Scanner) addString() {
 		parseError.RaiseError(scanner.line, "Unterminated string.")
 		return
 	}
-	scanner.advance()
+	scanner.advance() // Skip the closing quote.
 	value := scanner.source[scanner.start+1 : scanner.current-1]
 	scanner.addTokenWithLiteral(token.STRING, value)
 }
-func isDigit(c string) bool {
-	return c >= "0" && c <= "9"
+
+func isDigit(c byte) bool {
+	return c >= '0' && c <= '9'
 }
+
 func (scanner *Scanner) number() {
 	for isDigit(scanner.peek()) {
 		scanner.advance()
 	}
-	if scanner.peek() != "\"" && scanner.isAtEnd() {
-		scanner.advance()
+	// Look for a fractional part.
+	if scanner.peek() == '.' && isDigit(scanner.peekNext()) {
+		scanner.advance() // Consume the "."
 		for isDigit(scanner.peek()) {
 			scanner.advance()
 		}
 	}
 	scanner.addTokenWithLiteral(token.NUMBER, scanner.source[scanner.start:scanner.current])
 }
-func (scanner *Scanner) peekNext() string {
+
+func (scanner *Scanner) peekNext() byte {
 	if scanner.current+1 >= len(scanner.source) {
-		return "\\0"
+		return 0
 	}
-	return string(scanner.source[scanner.current+1])
+	return scanner.source[scanner.current+1]
 }
